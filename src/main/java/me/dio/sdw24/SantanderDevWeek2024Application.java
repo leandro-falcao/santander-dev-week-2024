8c942d8f2a9a492cef892f0d2072a8e69e292fab
leandro-falcao <leandrofalcaoti@gmail.com>
2024-03-31 14:08:24 -0300

    começando o projeto santader dev week de março de 2024 do santader na DIO | start project by santander in DIO, project santander dev week, good luck
    Signed-off-by: leandro-falcao <leandrofalcaoti@gmail.com>

---
+++ src/main/java/me/dio/sdw24/SantanderDevWeek2024Application.java
@@ -0,0 +1,14 @@
+package me.dio.sdw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+}
+//old name of class SantanderDevWeek2024Application
